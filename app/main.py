192ffca91e5751c40301a5949119db942334670c
FMaslina <FMaslina@yandex.ru>
2024-08-03 17:46:23 +0700

    Add get_hotels endpoint

--- app/main.py
+++ app/main.py
@@ -1,5 +1,15 @@
-from fastapi import FastAPI
+from datetime import date
+from typing import Optional
 
+from fastapi import FastAPI, Query
 
 app = FastAPI()
 
+
+@app.get("/hotels")
+def get_hotels(date_from: date,
+               date_to: date,
+               location: str,
+               has_spa: Optional[bool] = None,
+               stars: Optional[int] = Query(None, ge=1, le=5)):
+    return date_from, date_to
