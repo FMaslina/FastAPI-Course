6581524552d977d6b0326efa5367f200155e29f2
FMaslina <FMaslina@yandex.ru>
2024-08-03 18:10:25 +0700

    Add add_booking endpoint

--- app/main.py
+++ app/main.py
@@ -2,14 +2,41 @@ from datetime import date
 from typing import Optional
 
 from fastapi import FastAPI, Query
+from pydantic import BaseModel
 
 app = FastAPI()
 
 
-@app.get("/hotels")
+class SHotel(BaseModel):
+    address: str
+    name: str
+    stars: int
+
+
+@app.get("/hotels", response_model=list[SHotel])
 def get_hotels(date_from: date,
                date_to: date,
                location: str,
                has_spa: Optional[bool] = None,
                stars: Optional[int] = Query(None, ge=1, le=5)):
-    return date_from, date_to
+
+    hotels = [
+        {
+            "address": "Test",
+            "name": "Test name",
+            "stars": 5
+        }
+    ]
+
+    return hotels
+
+
+class SBooking(BaseModel):
+    room_id: int
+    date_from: date
+    date_to: date
+
+
+@app.post("/booking")
+def add_booking(booking: SBooking):
+    pass
